27058873c4c51a1687731c4683ba590dfb9bb96a
Yuzbekov Elvin <yuzbekov79@gmail.com>
2023-04-21 23:11:20 +0400

    change products line-clamp

--- src/components/products/NewProductItem.js
+++ src/components/products/NewProductItem.js
@@ -10,7 +10,7 @@ export default function NewProductItem({ product }) {
         <div className='box p-[35px 25px 25px]  py-[25px] bg-white relative shadow '>
             <div className='flex justify-center items-center flex-col '>
             <img className='max-w-[100%]  h-[150px] max-md:max-h-full' src={product.image} alt="" />
-            <span className='mt-[2rem] font-bold text-[#3abff8]'>{product.title}</span>
+            <p className='mt-[2rem] font-bold line-clamp-1 text-[#3abff8]'>{product.title}</p>
             <p className='absolute -bottom-3 bg-[#3abff8] hover:bg-[#089ddd] transition-colors text-white px-14 py-1 font-semibold text-[12px] rounded'>
                 {t('Ətraflı')}
                 </p>
